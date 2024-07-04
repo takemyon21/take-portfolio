e1bb1687a91051fffd1f6a24813eb0e6e44e2cf3
takemyon21 <diver_takemyon@hotmail.com>
2024-07-05 00:50:10 +0900

    レイアウト調整

--- src/components/ShowHtml.jsx
+++ src/components/ShowHtml.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 const ShowHtml = ({ html }) => {
-  return <div class="works__detail" dangerouslySetInnerHTML={{ __html: html }} />
+  return <div className="works__detail" dangerouslySetInnerHTML={{ __html: html }} />
 }
 
 export default ShowHtml
